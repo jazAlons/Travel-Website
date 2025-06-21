6806bcd9429ef00ac7049248eaa9f5542b2d182e
jazAlonso <jazminalonsobri@gmail.com>
2025-06-21 13:19:45 -0600

    add ScrollReveal animations to multiple sections including footer

--- main.js
+++ main.js
@@ -81,5 +81,8 @@ const sr = ScrollReveal({
     // reset: true // Animations repeat
 })
 
-sr.reveal(`.home__data`)
+sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`)
 sr.reveal(`.home__card`, {delay:600, distance:'100px', interval:100})
+sr.reveal(`.about__data, .join__image`, {origin:'right'})
+sr.reveal(`.about__image, .join__data`,{origin:'left'})
+sr.reveal(`.popular__card`,{interval: 200})
